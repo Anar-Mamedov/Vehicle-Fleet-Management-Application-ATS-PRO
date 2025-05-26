28b5e2185058dc933723c64000b536ebdea3ed4c
Anar Mamedov <45544776+Anar-Mamedov@users.noreply.github.com>
2025-05-26 17:31:51 +0400

    scrollbar problem is fixed

--- src/_root/pages/vehicles-control/yakit/YakitTablo.jsx
+++ src/_root/pages/vehicles-control/yakit/YakitTablo.jsx
@@ -1508,7 +1508,7 @@ const Yakit = ({ customFields }) => {
             padding: "10px",
             height: "calc(100vh - 200px)",
             borderRadius: "8px 8px 8px 8px",
-            filter: "drop-shadow(0px 2px 4px rgba(0,0,0,0.1))",
+            // filter: "drop-shadow(0px 2px 4px rgba(0,0,0,0.1))",
           }}
         >
           <Spin spinning={loading || (!infiniteScrollEnabled && paginationLoading)}>
