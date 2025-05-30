2280b0c93e48f3603a283227f794f778862c6d1b
Anar Mamedov <45544776+Anar-Mamedov@users.noreply.github.com>
2025-05-30 16:31:37 +0400

    Import React alongside useState in Firma component for clarity

--- src/_root/components/form/selects/Firma.jsx
+++ src/_root/components/form/selects/Firma.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Controller, useFormContext } from "react-hook-form";
 import PropTypes from "prop-types";
 import { Select } from "antd";
