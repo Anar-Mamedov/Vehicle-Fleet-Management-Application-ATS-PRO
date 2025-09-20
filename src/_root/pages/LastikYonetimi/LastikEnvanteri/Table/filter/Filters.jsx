994043df197fa3b33630b3fdd780a57cbf0c4aea
Anar Mamedov <45544776+Anar-Mamedov@users.noreply.github.com>
2025-09-20 22:54:11 +0400

    Enhance Filters component to support additional filter options
    - Added KodIDSelectbox components for 'tip' and 'ebat' selections, allowing users to filter results based on these criteria.
    - Updated state management to include new state variables for 'tipIds' and 'ebatIds', improving filter functionality.
    - Ensured adherence to ESLint rules for better code quality and maintainability.

--- src/_root/pages/LastikYonetimi/LastikEnvanteri/Table/filter/Filters.jsx
+++ src/_root/pages/LastikYonetimi/LastikEnvanteri/Table/filter/Filters.jsx
@@ -7,10 +7,14 @@ import PropTypes from "prop-types";
 // import ZamanAraligi from "./ZamanAraligi";
 import MarkaSelectbox from "../../../../../components/MarkaSelectbox";
 import ModelSelectbox from "../../../../../components/ModelSelectbox";
+import KodIDSelectbox from "../../../../../components/KodIDSelectbox";
+import { t } from "i18next";
 
 export default function Filters({ onChange }) {
   const [markaIds, setMarkaIds] = React.useState([]);
   const [modelIds, setModelIds] = React.useState([]);
+  const [tipIds, setTipIds] = React.useState([]);
+  const [ebatIds, setEbatIds] = React.useState([]);
   const [filters, setFilters] = React.useState({
     /* lokasyonlar: {},
     isemritipleri: {},
@@ -24,8 +28,10 @@ export default function Filters({ onChange }) {
       ...state,
       markaIds: Array.isArray(markaIds) ? markaIds : [],
       modelIds: Array.isArray(modelIds) ? modelIds : [],
+      tipIds: Array.isArray(tipIds) ? tipIds : [],
+      ebatIds: Array.isArray(ebatIds) ? ebatIds : [],
     }));
-  }, [markaIds, modelIds]);
+  }, [markaIds, modelIds, tipIds, ebatIds]);
 
   React.useEffect(() => {
     onChange("filters", filters);
@@ -34,8 +40,10 @@ export default function Filters({ onChange }) {
   return (
     <>
       <div style={{ display: "flex", gap: "10px" }}>
-        <MarkaSelectbox name1={"marka"} isRequired={false} onChange={setMarkaIds} dropdownWidth="300px" inputWidth="200px" multiSelect={true} />
-        <ModelSelectbox name1={"model"} isRequired={false} onChange={setModelIds} dropdownWidth="300px" inputWidth="200px" markaId={markaIds} multiSelect={true} />
+        <MarkaSelectbox name1={"marka"} isRequired={false} onChange={setMarkaIds} dropdownWidth="300px" inputWidth="150px" multiSelect={true} />
+        <ModelSelectbox name1={"model"} isRequired={false} onChange={setModelIds} dropdownWidth="300px" inputWidth="150px" markaId={markaIds} multiSelect={true} />
+        <KodIDSelectbox name1={"tip"} isRequired={false} onChange={setTipIds} placeholder={t("tip")} dropdownWidth="300px" inputWidth="150px" multiSelect={true} kodID="705" />
+        <KodIDSelectbox name1={"ebat"} isRequired={false} onChange={setEbatIds} placeholder={t("ebat")} dropdownWidth="300px" inputWidth="150px" multiSelect={true} kodID="702" />
         {/*<TypeFilter*/}
         {/*  onSubmit={(newFilters) =>*/}
         {/*    setFilters((state) => ({ ...state, isemritipleri: newFilters }))*/}
